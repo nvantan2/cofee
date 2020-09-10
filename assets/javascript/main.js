c59e32a9d6dc0e0a9f0954a0f41fee4072560829
nvantan2 <tannguyen12.07.sk@gmail.com>
2020-09-10 19:17:05 +0700

    chores

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   });
 
   $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 800);
+    $("html, body").animate({ scrollTop: 0 }, 1000);
     return false;
   });
 
